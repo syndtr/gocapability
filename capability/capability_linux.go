cf8e728cb069c5d9c8f146fa0cbda4fc1db049d3
Kir Kolyshkin <kolyshkin@gmail.com>
2023-02-15 12:52:52 -0800

    Load: simplify code, fix a linter warning
    Fix the following warning:
    
    > capability_linux.go:407:17: S1039: unnecessary use of fmt.Sprintf (gosimple)
    > 		status_path = fmt.Sprintf("/proc/self/status")
    >		              ^
    
    and simplify the code for the common case when pid is 0.
    
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- capability/capability_linux.go
+++ capability/capability_linux.go
@@ -401,11 +401,8 @@ func (c *capsV3) Load() (err error) {
 		return
 	}
 
-	var status_path string
-
-	if c.hdr.pid == 0 {
-		status_path = fmt.Sprintf("/proc/self/status")
-	} else {
+	status_path := "/proc/self/status"
+	if c.hdr.pid != 0 {
 		status_path = fmt.Sprintf("/proc/%d/status", c.hdr.pid)
 	}
 
